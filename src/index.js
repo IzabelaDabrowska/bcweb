e9f85da0364a61efd35efa8b214fe99bbb1e24af
Izabela <dabrowska.izabelaa@wp.pl>
2018-11-20 23:43:25 +0100

    Added expandable hamburger menu.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 window.onload = function () {
 
     var hamburger = document.getElementById('hamburger');
+    var mainContent = document.getElementById('main-content');
 
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('is-active');
+        mainContent.classList.toggle('expanded');
     })
 };
