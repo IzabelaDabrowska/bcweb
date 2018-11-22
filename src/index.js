19753d35f012114790eea0eaf926f1c30f908050
Izabela <dabrowska.izabelaa@wp.pl>
2018-11-22 23:49:43 +0100

    Added basic changing template to links list.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,38 @@ window.onload = function () {
 
     var hamburger = document.getElementById('hamburger');
     var mainContent = document.getElementById('main-content');
+    var content = document.getElementsByClassName('menu-content');
+    var links = document.getElementsByClassName('link-button');
+
+    var homeTemplate = '<div class="positioning-template"><div class="template-header">Strona Główna</div><div class="main-information-template"></div></div>';
+    var aboutTemplate = '<div class="positioning-template"><div class="template-header">O Nas</div><div class="main-information-template"></div></div>';
+    var offerTemplate = '<div class="positioning-template"><div class="template-header">Oferta</div><div class="main-information-template offer-template">' +
+        '<button class="change-template">Strony internetowe</button><button>Sklepy internetowe</button><button>Projektowanie graficzne</button>' +
+        '<button>Druk materiałów reklamowych</button><button>Prezentacje i animacje 2D</button><button>Copywriting</button></div></div>';
+    var realizationsTemplate = '<div class="positioning-template"><div class="template-header">Realizacje</div><div class="main-information-template"></div></div>';
+    var articlesTemplate = '<div class="positioning-template"><div class="template-header">Artykuły</div><div class="main-information-template"></div></div>';
+    var contactTemplate = '<div class="positioning-template"><div class="template-header">Kontakt</div><div class="main-information-template"></div></div>';
+
+    var sectionTemplateMap = {
+        home: homeTemplate,
+        about: aboutTemplate,
+        offer: offerTemplate,
+        realizations: realizationsTemplate,
+        articles: articlesTemplate,
+        contact: contactTemplate
+    };
 
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('is-active');
         mainContent.classList.toggle('expanded');
-    })
+    });
+
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        link.addEventListener('click', function (event) {
+            console.log(content);
+            var template = sectionTemplateMap[event.target.id];
+            content[0].innerHTML = template;
+        })
+    }
 };
